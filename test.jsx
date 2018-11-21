6522725fdf7d12277ffb211e87e264584fb59e82
jida <jakazzy@gmail.com>
2018-11-21 19:15:41 +0000

    test 10 - compare two arrays

--- test.jsx
+++ test.jsx
@@ -159,4 +159,14 @@ function findMissingLetter(array) {
   }
 }
 
+/* TEST 10 
+Are they the same. Compare two arrays such that array2 is the square of array1 
+*/
+/* TEST 10 - SOLUTION 1
+*/
+function comp(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
     
