90d60521e0d63220f127172fee0d05d1d6c4dcee
jida <jakazzy@gmail.com>
2018-11-21 18:23:56 +0000

    test 9 - find the missing letter

--- test.jsx
+++ test.jsx
@@ -1,7 +1,9 @@
 /*TEST 1
-Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters,
 
-each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+each taken only once - coming from s1 or s2.
+ #Examples: ``` a = "xyaabbbccccdefww" 
+ b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
 */
 function longest(s1, s2) {
@@ -47,8 +49,8 @@ function addBinary(a,b) {
 
   /*solution 3 */
   function addBinary(a,b){
-    if (a.isIntiger&& b.isIntiger){ //if all argument is intiger digits
-      return (a+b).toSting(2); //ok , return a string
+    if (a.isIntiger&& b.isIntiger){ //if all argument is integer digits
+      return (a+b).toString(2); //ok , return a string
       } else {
       return (parseInt(a,10)+parseInt(b,10)).toString(2); //if some of argument send as a string - parse values to 10 digits int. values and return as a binary 
        }
@@ -116,3 +118,45 @@ function list(arr) {
   .join(", ") // Join with ", "
   .replace(/, (?!.*, )/, " & "); // Replace last ", " with " & "
 }
+
+/* TEST 8 - SOLUTION 1
+convert to title case*/
+function charCase(str){
+  str = str.toLowerCase();
+  str = str.split(" ");
+  result = [];
+  for (letter of str){result.push(letter[0].toUpperCase() + letter.slice(1))} return result.join(" ");
+  }
+
+  /*TEST 9
+  fIND THE MISSING LETTER */
+  /* TEST 9 - SOLUTION 1
+*/
+function findMissingLetter(array){
+  let arrays = array.toString("").toLowerCase().split(",");
+  //create a variable of alphabets caled alph
+  //convert to array
+  //Take the beginning and end elements of the test array slice alph using the test arrays beginning and end
+  //compare both to identify the missing element.
+  let alph = "abcdefghijklmnopqrstuvwxyz".split("");
+  let firstLett = arrays[0]; 
+  let lastLett = arrays[arrays.length -1];
+  let newArray =alph.slice(alph.indexOf(firstLett), alph.indexOf(lastLett) +1);
+  let missingLett = "";
+  for (let x = 0; x < newArray.length; x++){
+  if (!(arrays.includes(newArray[x]))){ missingLett += newArray[x]}}
+  if (array[0] == array[0].toUpperCase()){
+  return missingLett.toUpperCase();} else return missingLett;
+  }
+  /* TEST 9 - SOLUTION 2
+*/
+function findMissingLetter(array) {
+  var string = array.join("");
+  for (var i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i + 1) - string.charCodeAt(i) != 1) {
+      return String.fromCharCode(string.charCodeAt(i) + 1);
+    }
+  }
+}
+
+    
